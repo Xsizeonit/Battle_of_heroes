84e5885499c3664e08c87fa1bb185b17c6d6bea0
Xsizeonit <anatoly.tsykunov@yandex.ru>
2022-12-03 17:02:33 +0300

    Add simple post request handle on server part

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+    "encoding/json"
     "log"
     "net/http"
+    "io/ioutil"
 )
 
-func main() {
-    log.Fatal(http.ListenAndServe(":3333", http.FileServer(http.Dir("."))))
+type test_struct struct {
+    Test string
 }
+
+func test(rw http.ResponseWriter, req *http.Request) {
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        panic(err)
+    }
+    log.Println(string(body))
+    var t test_struct
+    err = json.Unmarshal(body, &t)
+    if err != nil {
+        panic(err)
+    }
+    rw.Write(body);
+}
+
+func main() {
+    http.HandleFunc("/", test)
+    log.Fatal(http.ListenAndServe(":3333", nil))
+}
+
